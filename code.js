fd80edb6a8e4d3093adb269b7f641ac9b9716114
DamienKD <104658974+DamienKD@users.noreply.github.com>
2023-04-27 17:34:26 +1000

    Add files via upload

---
+++ code.js
@@ -0,0 +1,55 @@
+text = "";
+background = "";
+textSize = "";
+x = ""
+y = "";
+
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized')
+}
+
+
+function gotPoses()
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+
+    }
+}
+
+function draw()
+{
+    background(canvas);
+    textSize();
+    fill()
+    text('Damien Desveaux');
+    text(x="12,20,30,40,50,60,70,80,90,100");
+    text(y="10, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000");
+}
